6073e88e6fab62ce240e724baa65e5848d945bb6
romain nicod <ai-gmented@pm.me>
2026-08-12 14:51:06 +0200

    message

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,3 +1,6 @@
 class Rating < ApplicationRecord
-  belongs_to :restaurant
-end
+belongs_to :restaurant
+
+  validates :content, presence: true
+  validates :rating, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 5 }
+endend
